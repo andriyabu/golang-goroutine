a4d564d4ab546155a43d5d8000c6301695231ca1
ANDRI YABU NDAPA REHING <andriyabu@gmail.com>
2022-02-25 15:09:45 +0700

    create DisplayNumber Function and Test to display loop 1000 times with concurency

---
+++ goroutine_test.go
@@ -0,0 +1,39 @@
+package golanggoroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func RunHelloWorld() {
+	for i := 0; i < 100; i++ {
+		fmt.Println("Helloo GoWorld", i)
+	}
+}
+
+func RunHelloGoroutine() {
+	for i := 0; i < 100; i++ {
+		fmt.Println("Helloo Goroutine", i)
+	}
+}
+
+func TestCreateGoroutine(t *testing.T) {
+	go RunHelloWorld()
+	go RunHelloGoroutine()
+	fmt.Println("Ups")
+
+	time.Sleep(1 * time.Second)
+}
+
+func DisplayNumber(number int) {
+	fmt.Println("Display ", number)
+}
+
+func TestManyGoroutine(t *testing.T) {
+	for i := 0; i < 1000; i++ {
+		go DisplayNumber(i)
+	}
+
+	time.Sleep(5 * time.Second)
+}
